2d05aa9ade6e682da949db5c6d44e441b84a82cb
Diego Freitas de Sousa <diego.freitas.professional@outlook.com>
2024-09-12 12:40:42 -0300

    port

--- index.js
+++ index.js
@@ -2,16 +2,18 @@ const express = require('express');
 const cors = require('cors')
 const { testDBConnection, getStations } = require('./data/database');
 const app = express();
-const port = 555;
+const port = 8999;
 
 
 app.use(cors())
 
 app.get('/stations', async (req, res) => {
+  console.log('sim');
+  
     let response = await getStations(req.query)
     res.send(JSON.stringify(response));
 });
 
-app.listen(80, () => {
+app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
