b1b922d2c1e54739c2567808f1a6d0e6b3fe5bd6
Diego Freitas <diego.freitas.professional@outlook.com>
2024-11-12 18:46:33 -0300

    corrigindo stations where

--- models/stationModel.js
+++ models/stationModel.js
@@ -1,30 +1,24 @@
+const { buildClause } = require("../helpers/generalHelper")
+
 const buildWhere = (params, query) =>{
     let whereRaw = []
 
-    const buildClause = (param_name, table_field_name, compare_type) =>{
-        let value = params[param_name]
-        if(value){
-            value = compare_type === 'like' ? `%${value}%` : value
-            whereRaw.push(`${table_field_name} ${compare_type} '${value}'`)
-        }
-    }
-
     //construindo cláusulas analisadoras dos parâmetros passados
-    buildClause('prefix', 'prefix', '=')
-    buildClause('name', 'stations.name', 'like')
-    buildClause('station_type', 'station_types.name', '=')
-    buildClause('station_type_id', 'station_type_id', '=')
-    buildClause('city_id', 'stations.city_id', '=')
-    buildClause('cod_ibge', 'cities.cod_ibge', '=')
-    buildClause('ugrhi_id', 'stations.ugrhi_id', '=')
-    buildClause('ugrhi_cod', 'ugrhis.cod', '=')
-    buildClause('ugrhi_name', 'ugrhis.name', 'like')
-    buildClause('subugrhi_id', 'stations.subugrhi_id', '=')
-    buildClause('station_owner_id', 'station_prefixes.station_owner_id', '=')
-    buildClause('station_owner_ids', 'station_prefixes.station_owner_id', 'in')
-    buildClause('station_owner', 'station_owners.name', '=')
+    whereRaw.push(buildClause(params,'prefix', 'prefix', '='))
+    whereRaw.push(buildClause(params,'name', 'stations.name', 'like'))
+    whereRaw.push(buildClause(params,'station_type', 'station_types.name', '='))
+    whereRaw.push(buildClause(params,'station_type_id', 'station_type_id', '='))
+    whereRaw.push(buildClause(params,'city_id', 'stations.city_id', '='))
+    whereRaw.push(buildClause(params,'cod_ibge', 'cities.cod_ibge', '='))
+    whereRaw.push(buildClause(params,'ugrhi_id', 'stations.ugrhi_id', '='))
+    whereRaw.push(buildClause(params,'ugrhi_cod', 'ugrhis.cod', '='))
+    whereRaw.push(buildClause(params,'ugrhi_name', 'ugrhis.name', 'like'))
+    whereRaw.push(buildClause(params,'subugrhi_id', 'stations.subugrhi_id', '='))
+    whereRaw.push(buildClause(params,'station_owner_id', 'station_prefixes.station_owner_id', '='))
+    whereRaw.push(buildClause(params,'station_owner_ids', 'station_prefixes.station_owner_id', 'in'))
+    whereRaw.push(buildClause(params,'station_owner', 'station_owners.name', '='))
 
-    if(whereRaw){
+    if(whereRaw.length > 1){
         query.whereRaw(whereRaw.join(' and '))
     }
 }
