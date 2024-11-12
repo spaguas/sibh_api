255b81a713b6c699a236638d38fed737244b1586
Diego Freitas <diego.freitas.professional@outlook.com>
2024-11-12 19:07:13 -0300

    inserindo alt_prefix no serializer

--- serializers/serializer.js
+++ serializers/serializer.js
@@ -3,8 +3,8 @@ const serializer = {
         very_short: ['id', 'prefix', 'station_type_id'], //sem join, apenas para identificação de id, posto, tipo_id
         short: ['id', 'prefix', 'station_type_id', 'station_owner_id', 'operation_status', 'date_last_measurement'], //sem join, mais dados do posto
         all_clean: ['id', 'prefix', 'alt_prefix', 'station_type_id', 'station_owner_id', 'operation_status', 'transmission_gap', 'measurement_gap', 'date_last_measurement', 'public', 'battery_linear'], //sem join, todas as colunas
-        default: ['station_prefixes.id', 'prefix', 'stations.name', 'station_types.name as station_type', 'stations.latitude', 'stations.longitude', 'cities.id as city_id',  'cities.name as city_name', 'cities.cod_ibge', 'ugrhis.id as ugrhi_id', 'ugrhis.name as ugrhi_name', 'ugrhis.cod as ugrhi_cod', 'subugrhis.name as subugrhi', 'station_owners.name as station_owner', 'date_last_measurement'], //com join, colunas necessárias para identificação e localização do posto,
-        complete: ['station_prefixes.id', 'prefix', 'stations.name', 'station_types.name as station_type', 
+        default: ['station_prefixes.id', 'alt_prefix', 'prefix', 'stations.name', 'station_types.name as station_type', 'stations.latitude', 'stations.longitude', 'cities.id as city_id',  'cities.name as city_name', 'cities.cod_ibge', 'ugrhis.id as ugrhi_id', 'ugrhis.name as ugrhi_name', 'ugrhis.cod as ugrhi_cod', 'subugrhis.name as subugrhi', 'station_owners.name as station_owner', 'date_last_measurement'], //com join, colunas necessárias para identificação e localização do posto,
+        complete: ['station_prefixes.id', 'alt_prefix', 'prefix', 'stations.name', 'station_types.name as station_type', 
             'stations.id as station_id', 'stations.latitude', 'stations.longitude', 'cities.id as city_id',  
             'cities.name as city_name', 'cities.cod_ibge', 'ugrhis.id as ugrhi_id', 'ugrhis.name as ugrhi_name', 
             'ugrhis.cod as ugrhi_cod', 'subugrhis.name as subugrhi', 'station_owners.name as station_owner', 
