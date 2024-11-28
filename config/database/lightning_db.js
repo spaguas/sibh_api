b65c3478755275cf6eee2bfc96cdb237d07cbbdf
Diego Freitas de Sousa <diego.freitas.professional@outlook.com>
2024-11-28 16:32:40 -0300

    raios apenas de SP

--- config/database/lightning_db.js
+++ config/database/lightning_db.js
@@ -2,9 +2,14 @@ const { pgWD } = require("../knex")
 
 const getLightnings = async () =>{
     
-    let query = pgWD.table('lightning_events').limit(100).orderByRaw('datetime desc')
+    let query = pgWD.table('lightning_events')
     
-    // buildWhere(options, query)    
+    // buildWhere(options, query)  
+    
+    query.whereRaw('latitude between -26.106681 and -18.890425 and longitude between -54.446606 and -41.525717') //bounds pouco maior que o estado de SP
+    query.whereRaw("datetime >= now() - interval '2 hours'")
+
+    query.orderByRaw('datetime desc')
 
     return query
     
