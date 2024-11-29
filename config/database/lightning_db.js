a85f623bdeeb5b3ae8fb18f12beba26f30f05533
Diego Freitas de Sousa <diego.freitas.professional@outlook.com>
2024-11-29 15:38:24 -0300

    atualizando query do lightning

--- config/database/lightning_db.js
+++ config/database/lightning_db.js
@@ -7,7 +7,8 @@ const getLightnings = async () =>{
     // buildWhere(options, query)  
     
     query.whereRaw('latitude between -26.106681 and -18.890425 and longitude between -54.446606 and -41.525717') //bounds pouco maior que o estado de SP
-    query.whereRaw("datetime >= now() - interval '2 hours'")
+    query.whereRaw("datetime >= now() - interval '1 hour'")
+    query.whereRaw("quality_control_atd <= 1")
 
     query.orderByRaw('datetime desc')
 
