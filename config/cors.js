2bf840ea36dfe9e3f9811c6b49e6ada6b71e0570
Diego Freitas de Sousa <diego.freitas.professional@outlook.com>
2024-12-18 15:34:46 -0300

    adicionando user-token header permit

--- config/cors.js
+++ config/cors.js
@@ -1,7 +1,7 @@
 const corsOptions = {
     origin: '*', // Permite qualquer origem
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Métodos permitidos
-    allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
+    allowedHeaders: ['Content-Type', 'Authorization', 'user-token'], // Cabeçalhos permitidos
     exposedHeaders: ['X-Custom-Header'], // Cabeçalhos expostos ao cliente
     // credentials: false, // Não permite envio de cookies ou credenciais
     optionsSuccessStatus: 204 // Status padrão para respostas pré-flight bem-sucedidas
