b48acdc3ec515ee3acefad317a987b5fab4ab8cd
Diego Freitas de Sousa <diego.freitas.professional@outlook.com>
2025-01-30 22:08:30 -0300

    corrigindo new alert exception

--- routes/alertRoutes.js
+++ routes/alertRoutes.js
@@ -9,9 +9,16 @@ router.get('/',  async (req, res) => {
     let alerts = await getAlerts(req.query)
     res.send(alerts);
 });
-router.post('/new', async (req, res) => {   
-    let result = await insertAlert(req.body)
-    res.status(result.status).send(result);
+router.post('/new', async (req, res) => {  
+    let result 
+    try{
+        result = await insertAlert(req.body)
+    } catch (e){
+        console.log(e);
+        res.status(400).send({error: 'Erro ao inserir alerta'})
+    }
+    
+    res.status(result.status).send(result)
 });
 
 module.exports = router
