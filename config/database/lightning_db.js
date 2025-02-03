fb1857171ddb11fffa303aa420fc687ea4d00f5a
Diego Freitas de Sousa <diego.freitas.professional@outlook.com>
2025-02-03 16:46:21 -0300

    lightning baixada

--- config/database/lightning_db.js
+++ config/database/lightning_db.js
@@ -1,20 +1,20 @@
 const { pgWD } = require("../knex")
 
-const getLightnings = async () =>{
+const getLightnings = async (bounds) =>{
     
     let query = pgWD.table('lightning_events')
     
-    // buildWhere(options, query)  
-    
-    query.whereRaw('latitude between -26.106681 and -18.890425 and longitude between -54.446606 and -41.525717') //bounds pouco maior que o estado de SP
+    query.whereRaw(`latitude between ${bounds[0]} and ${bounds[1]} and longitude between ${bounds[2]} and ${bounds[3]}`) //bounds pouco maior que o estado de SP
     query.whereRaw("datetime >= now() - interval '1 hour'")
     query.whereRaw("quality_control_atd <= 1")
 
     query.orderByRaw('datetime desc')
 
-    return query
+    console.log(query.toSQL());
     
 
+    return query
+    
 }
 
 module.exports = {
--- routes/lightningRoutes.js
+++ routes/lightningRoutes.js
@@ -5,7 +5,12 @@ const router = express.Router();
 
 
 router.get('/last_lightnings', async (req, res) => {  
-    let response = await getLightnings()
+    let response = await getLightnings([-26.106681, -18.890425, -54.446606, -41.525717])
+    res.send(response);
+});
+
+router.get('/last_lightnings/baixada', async (req, res) => {  
+    let response = await getLightnings([-45.145719, -23.019560, -47.194670, -24.607547])
     res.send(response);
 });
 
