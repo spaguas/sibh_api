09f5ee40bcc8db275f2683a336b626450cbc9478
Diego Freitas <diego.freitas.professional@outlook.com>
2025-03-01 14:59:55 -0300

    temp max e min

--- serializers/serializer.js
+++ serializers/serializer.js
@@ -40,7 +40,7 @@ const serializer = {
         default: ['id', 'email', 'name', 'encrypted_password']
     },
     hidroapp:{
-        default: ['id', 'model_type', 'model_id', 'date_hour', 'rain', 'spi_6', 'dm', 'ndvi', 'temp']
+        default: ['id', 'model_type', 'model_id', 'date_hour', 'rain', 'spi_6', 'dm', 'ndvi', 'temp', 'temp_max', 'temp_min']
     }
 }
 
